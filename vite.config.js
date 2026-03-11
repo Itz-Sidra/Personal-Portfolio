713756eeb130619180d8ad131c555596c30caa73
JAHANGIR SIDRA TABREZ <sidrajahangir93@gmail.com>
2026-03-11 23:09:22 +0530

    fix: deploy correct build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change '/sidra-portfolio/' to your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
   base: '/Personal-Portfolio/',
